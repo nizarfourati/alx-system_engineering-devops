760f832402e88fae786586cff01d6ba3d9ad07de
nizarfourati <nizarfourati.13@gmsil.com>
2024-06-11 21:59:43 +0000

    push

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-0-subs.py
+this doc for module
 """
 import requests
 
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+
+
 def number_of_subscribers(subreddit):
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {
-	   "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"	    	    }
-	response = requests.get(url, headers=headers, allow_redirects=False)
-	
-	if response.status_code == 404:
-		return 0
-	results = response.json().get("data")
-	return results.get("subscribers")
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
